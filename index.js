5914339d85c154aa71a85765132f25d2f7b3bfa0
anthony bell <anthonybell67.ab@gmail.com>
2024-05-11 19:40:26 -0400

    fixed questions array

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
 // TODO: Include packages needed for this application
+// TODO: Create a function to write README file
+const fs = require('fs');
+const inquirer = require("inquirer")
+const path = require("path")
+const generateMarkdown = require("./utils/generateMarkdown");
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 // TODO: Create an array of questions for user input
-const questions = ["What is the title of your project?", "What motivated you to create this project?", 
-"What problem does this project solve?", "What makes your project stand out?", "What features will your project include in the future?"];
+const questions = [
+    {  
+      type: 'input',
+      name: 'title',
+      message: 'What is your user name?',
+    }
+    {  
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?',
+         },
+         {  
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email?',
+             }
+          
+    {  
+      type: 'input',
+      name: 'description',
+      message: 'What is the purpose and functionality of this project?',
+         },
+         {  
+        type: 'input',
+        name: 'install',
+        message: 'How do you install the project?',             
+       }
+             {  
+                type: 'checkbox',
+                name: 'license',
+                message: 'please select a license for this project?',
+                Choices: ["MIT", "Apache2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+                 }
+             ];
 
 
-// TODO: Create a function to write README file
-const fs = require('fs')
-const Database = ('./package-lock.json') => {
-    return new promise((resolve, reject) => {
-fs.readFile('./package-lock.json', 'utf-8', (err, data) => {
-    err ? reject(err) : resolve(data)
+ 
+const loadDatabase = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./package-lock.json', 'utf-8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
+ 
 
-})
-})
-
-fs.writeFile('ReadME.md', 'Readme Generator', (err) => {
+fs.writeFile('ReadME.md', 'Welcome to our Readme Generator!', (err) => {
 if (err) {
     throw new error(err);
 }
@@ -23,7 +65,7 @@ console.log("File was written succesfully");
 })
 // TODO: Create a function to initialize app
 const start = async () => {
-   try {
+   try { 
     const ReadmeGenerator = await loadDatabase('./package-lock.json'); 
    } catch (error) {
 
