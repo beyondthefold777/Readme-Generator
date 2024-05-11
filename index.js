eb6b4e34ab55fac38567dcb81a9171b4fa0ba4a2
anthony bell <anthonybell67.ab@gmail.com>
2024-05-11 01:12:35 -0400

    downloaded packages and created an array of questions for user input in the app

---
+++ index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = ["What is your project title?", "What motivated you to create this project?", 
+"What problem does this project solve?", "What makes your project stand out?", "What features will the app encompse in the future?"];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
