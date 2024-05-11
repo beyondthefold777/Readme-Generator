8251dc0feba22a2d4e3924c6f00c488ce2dda0db
anthony bell <anthonybell67.ab@gmail.com>
2024-05-11 02:54:30 -0400

    writing function to write readme from the file system

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = ["What is your project title?", "What motivated you to create this project?", 
-"What problem does this project solve?", "What makes your project stand out?", "What features will the app encompse in the future?"];
+const questions = ["What is the title of your project?", "What motivated you to create this project?", 
+"What problem does this project solve?", "What makes your project stand out?", "What features will your project include in the future?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const fs = require('fs')
+fs.writeFile('./package-lock.json', 'utf-8', (err, data) => {
+if (err) {
+    console.log(err);
+} else {
+    console.log(data);
+}
+});
+
+
 
 // TODO: Create a function to initialize app
 function init() {}
